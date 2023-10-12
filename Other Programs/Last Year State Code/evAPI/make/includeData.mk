0c15fbcf53d4fbe3a8b421903910a832fcd2845c
Chez <teonoahcarrion@hotmail.com>
2023-10-12 13:50:23 +0000

    Cleared branch

--- Other Programs/Last Year State Code/evAPI/make/includeData.mk
+++
@@ -1,9 +0,0 @@
-# Include made data for compiling the evAPI
-
-SRC_H += $(wildcard evAPI/*.h)
-SRC_H += $(wildcard evAPI/Common/include/*.h)
-SRC_H += $(wildcard evAPI/customVexDevices/Control/include/*.h)
-SRC_H += $(wildcard evAPI/customVexDevices/Sensors/include*/.h)
-SRC_H += $(wildcard evAPI/odomSupport/include/*.h)
-SRC_H += $(wildcard evAPI/robotControl/include/*.h)
-SRC_H += $(wildcard evAPI/vexUI/include/*.h)
--- VEX-LVGL-UI-Testing/include/lvgl.h
+++
@@ -1,8 +0,0 @@
-//File that allows icon files to properly work without editing.
-
-#ifndef LVGL_FIX_H
-#define LVGL_FIX_H
-
-#include "../lvgl/lvgl.h"
-
-#endif // LVGL_FIX_H
